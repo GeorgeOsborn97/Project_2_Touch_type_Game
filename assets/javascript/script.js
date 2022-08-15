ccc0fd9e069b7f1cae580bb2427c46f6d5b0b822
GeorgeOsborn97 <George.b.osborn@outlook.com>
2022-08-15 15:39:56 +0000

    first attempt at random key generation

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,5 @@
+let keyArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
 function usernameEntry(event) {
     event.preventDefault();
     let username = document.getElementById("username");
@@ -22,15 +24,20 @@ function usernameEntry(event) {
     let screen = document.getElementById("menu-game-screen");
     screen.appendChild(title);
     screen.appendChild(instructions);
-    screen.appendChild(play)
+    screen.appendChild(play);
 
 }
 let form = document.getElementById("name-select");
 form.addEventListener('submit', usernameEntry);
 
 function startTheGame(event) {
-    title.innerHTML = "3";
+    title.innerHTML = Math.random() * 1000;
     instructions.remove();
     playButton.remove();
 
 }
+function randomKey(){
+   let i = Math.random() * 100
+   keyArray[i]
+}
+

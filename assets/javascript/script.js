3abec59992cc59fd9942312c5b8c906540b73438
GeorgeOsborn97 <George.b.osborn@outlook.com>
2022-08-15 13:08:22 +0000

    insert functions to change the menu on button clicks and submit the username

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,10 +2,35 @@ function usernameEntry(event) {
     event.preventDefault();
     let username = document.getElementById("username");
     let user = username.value;
-    let leaderboard = document.getElementById("leaderboard")
-    let newName = document.createElement("li")
+    let leaderboard = document.getElementById("leaderboard");
+    let newName = document.createElement("li");
     newName.innerHTML = user
-    leaderboard.appendChild(newName)
+    leaderboard.appendChild(newName);
+    form.remove();
+    let title = document.createElement("h1");
+    title.setAttribute('id', 'title');
+    let instructions = document.createElement("p");
+    instructions.setAttribute('id', 'instructions')
+    let play = document.createElement("button");
+    play.style.height = '50px';
+    play.style.width = '100px';
+    play.innerHTML = "PLAY";
+    play.setAttribute('onclick', 'startTheGame();')
+    play.setAttribute('id', 'playButton');
+    title.innerHTML = "Title"
+    instructions.innerHTML = "instructions to be added"
+    let screen = document.getElementById("menu-game-screen");
+    screen.appendChild(title);
+    screen.appendChild(instructions);
+    screen.appendChild(play)
+
 }
 let form = document.getElementById("name-select");
 form.addEventListener('submit', usernameEntry);
+
+function startTheGame(event) {
+    title.innerHTML = "3";
+    instructions.remove();
+    playButton.remove();
+
+}

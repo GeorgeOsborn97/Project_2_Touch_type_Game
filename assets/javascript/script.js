c88ba382b4e3818198eab9ef7fef7aca60ac6fc6
GeorgeOsborn97 <George.b.osborn@outlook.com>
2022-08-18 12:08:42 +0000

    add a sort function for the table

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -164,6 +164,7 @@ function endGame() {
         newScore.innerHTML = finalScore
         console.log(newScore)
         gameData.appendChild(newScore);
+
     //create a button that returns back to the original user inout for replayability
     let returnToMenuDiv = document.getElementById("menu-game-screen");
         returnToMenuDiv.setAttribute('onclick', 'resetMenu();');
@@ -212,5 +213,41 @@ function resetMenu(event) {
         console.log(i)
     let failReset = document.getElementsByClassName('fail');
     failReset.namedItem(i).style.color = "greenyellow"
+    failReset.namedItem(i).style.borderColor = "white"
+    sortTable();
     }
+    function sortTable() {
+        var table, rows, switching, z, x, y, shouldSwitch;
+        table = document.getElementById("leaderboard");
+        switching = true;
+        /*Make a loop that will continue until
+        no switching has been done:*/
+        while (switching) {
+          //start by saying: no switching is done:
+          switching = false;
+          rows = table.rows;
+          /*Loop through all table rows (except the
+          first, which contains table headers):*/
+          for (z = 1; z < (rows.length - 1); z++) {
+            //start by saying there should be no switching:
+            shouldSwitch = false;
+            /*Get the two elements you want to compare,
+            one from current row and one from the next:*/
+            x = rows[z].getElementsByTagName("td")[1];
+            y = rows[z + 1].getElementsByTagName("td")[1];
+            //check if the two rows should switch place:
+            if (Number(x.innerHTML) < Number(y.innerHTML)) {
+                //if so, mark as a switch and break the loop:
+                shouldSwitch = true;
+              break;
+            }
+          }
+          if (shouldSwitch) {
+            /*If a switch has been marked, make the switch
+            and mark that a switch has been done:*/
+            rows[z].parentNode.insertBefore(rows[z + 1], rows[z]);
+            switching = true;
+          }
+        }
+      }
 }

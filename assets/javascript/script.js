6900bf2b2ea55aace1fc60b47fcd1d860c9524ec
GeorgeOsborn97 <George.b.osborn@outlook.com>
2022-08-19 11:09:35 +0000

    add in missing semicolons

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,17 +1,16 @@
 // Define the arrays that will be required
-let keyArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-let userArray= ['Mark', 'George', 'Ben', 'Chris']
+let keyArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+let userArray= ['Mark', 'George', 'Ben', 'Chris'];
 // styling for the first and last fail blocks as css struggles to target id if its numerical
-let firstFail = document.getElementById('1')
-let fifthFail = document.getElementById('5')
-fifthFail.style.marginRight = "0px"
+let fifthFail = document.getElementById('5');
+fifthFail.style.marginRight = "0px";
 // responsive styling for different screen sizes
 var mq = window.matchMedia( "(max-width: 800px)" );
 if(mq.matches) {
     function mobileLoad(event){
         alert('Turn Device Portrait');
     }
-    document.addEventListener('DOMContentLoaded', mobileLoad)
+    document.addEventListener('DOMContentLoaded', mobileLoad);
     let scoreboard = document.getElementById('scoreboard');
     scoreboard.remove();
     let newScoreboard = document.createElement('div');
@@ -36,40 +35,40 @@ if(mq.matches) {
         <td class="table-data">Crs:</td>
         <td class="table-data">24</td>
     </tr>
-</table>`
+</table>`;
     let body = document.getElementById('home-page');
     body.appendChild(newScoreboard);
-    body.style.width = "95vw"
+    body.style.width = "95vw";
 }
 //First function that executes when a username is submitted. It's Purpose is to Remove the Form element, add an <li> element with the inputted name.
 //And create an instructions page witha play button that will begin the game.
 function usernameEntry(event) {
     var mw = window.matchMedia( "(max-width: 360px)" );
 if(mw.matches){
-    let counter = document.getElementById('counter')
-    counter.style.marginTop = "120px"
+    let counter = document.getElementById('counter');
+    counter.style.marginTop = "120px";
 }
     event.preventDefault();
     let username = document.getElementById("username");
     let user = username.value;
-        userArray.push(user)
+        userArray.push(user);
         form.remove();
     let title = document.createElement("h1");
         title.setAttribute('id', 'title');
-        title.style.color = "red"
+        title.style.color = "red";
     let instructions = document.createElement("p");
-        instructions.setAttribute('id', 'instructions')
+        instructions.setAttribute('id', 'instructions');
     let play = document.createElement("img");
         play.style.height = '100px';
         play.style.width = '150px';
         play.setAttribute('src','assets/images/pixilart-drawing-(play).png');
-        play.setAttribute('type', 'image')
+        play.setAttribute('type', 'image');
         play.setAttribute('onclick', 'startTheGame();');
         play.setAttribute('id', 'playButton');
-        title.innerHTML = "How to play"
-        title.style.color = "white"
-        instructions.innerHTML = "After the countdown a random key will be displayed in the circle, you will have 1 second to hit the same key before it changes. A correct click = 1 point, 5 mis-clicks and the game ends. there is a max score of 40 if you make it to the end. Good Luck!"
-        instructions.style.color = "white"
+        title.innerHTML = "How to play";
+        title.style.color = "white";
+        instructions.innerHTML = "After the countdown a random key will be displayed in the circle, you will have 1 second to hit the same key before it changes. A correct click = 1 point, 5 mis-clicks and the game ends. there is a max score of 40 if you make it to the end. Good Luck!";
+        instructions.style.color = "white";
         let screen = document.getElementById("menu-game-screen");
         screen.appendChild(title);
         screen.appendChild(instructions);
@@ -82,32 +81,32 @@ function startTheGame(event) {
     if(mq.matches){
         let header = document.getElementById('logo');
         header.remove();
-        let failDiv = document.getElementById('fail-div')
-        failDiv.style.paddingTop = '25px'
+        let failDiv = document.getElementById('fail-div');
+        failDiv.style.paddingTop = '25px';
     }
     var mw = window.matchMedia( "(max-width: 360px)" );
     if(mw.matches){
-        let counter = document.getElementById('counter')
-        counter.style.marginTop = "10px"
+        let counter = document.getElementById('counter');
+        counter.style.marginTop = "10px";
     }
-    title.style.padding = "80px, 80px, 80px, 80px"
-    title.style.fontSize = "120px" 
-    title.innerHTML = "GO!"
+    title.style.padding = "80px, 80px, 80px, 80px";
+    title.style.fontSize = "120px";
+    title.innerHTML = "GO!";
     //This function produces the random key and chnages the color every new key in order to keep a change if the random key is the same as the last
     // it also keeps a track of the amount of keys shown when the count hits 40 the game ends.
     function newKey() {
-        timesRun += 1
-        console.log(timesRun)
+        timesRun += 1;
+        console.log(timesRun);
         if (timesRun === 41){
             clearInterval(interval);  
             body.removeEventListener('keydown', keyPress); 
             endGame();     
         }
         //this is not functioning at the moment, look in the future at increasing speed every 10 rounds
-        roundCount += 1
+        roundCount += 1;
         if (roundCount === 10){
-            sec = sec * 0.9
-            roundCount = 0
+            sec = sec * 0.9;
+            roundCount = 0;
         }
        //This condition produces the random key and chnages the color every new key in order to keep a change if the random key is the same as the last
        if (timesRun != 40){
@@ -115,17 +114,17 @@ function startTheGame(event) {
             console.log(i);
             title.innerHTML = keyArray[i];
             if(title.style.color == "red"){
-                title.style.color = "blue"
+                title.style.color = "blue";
             } else {
-                title.style.color = "red"
+                title.style.color = "red";
             }
         }
     }
-    var timesRun = 0
-    var roundCount = 0
-    var sec = 1000
-    var failCount = 0
-    failArray = []
+    var timesRun = 0;
+    var roundCount = 0;
+    var sec = 1000;
+    var failCount = 0;
+    failArray = [];
     var interval = setInterval(newKey, sec); 
     instructions.remove();
     playButton.remove();
@@ -134,17 +133,16 @@ function startTheGame(event) {
         if (event.key == title.innerHTML) {
             console.log(event.key);
             let score = document.getElementById("counter");
-            score.innerHTML++
+            score.innerHTML++;
         } else {
             var audio = new Audio("assets/audio/14477_1460037267.mp3");
             audio.play();
-            failCount += 1
-            let failSigns = document.getElementById(failCount)
-            failSigns.style.color = "red"
-            failSigns.style.borderColor = "black"
-            failArray.push(failCount)
+            failCount += 1;
+            let failSigns = document.getElementById(failCount);
+            failSigns.style.color = "red";
+            failSigns.style.borderColor = "black";
+            failArray.push(failCount);
             if(failArray.push() == 5){
-                
                 clearInterval(interval);  
             body.removeEventListener('keydown', keyPress); 
             endGame();     
@@ -163,51 +161,51 @@ function endGame() {
     title.remove();
     console.log("end reached");
     let score = document.getElementById("counter");
-    var finalScore = score.innerHTML
-        console.log(userArray)
-    let x = userArray.push() - 1
-        console.log(userArray[x] + ': ' + finalScore)
+    var finalScore = score.innerHTML;
+        console.log(userArray);
+    let x = userArray.push() - 1;
+        console.log(userArray[x] + ': ' + finalScore);
     let leaderboard = document.getElementById("leaderboard");
-    let newRow = document.createElement("tr")
-        leaderboard.appendChild(newRow)
-        newRow.setAttribute('id', 'userInput')
-    const gameData = document.querySelector('#userInput')
+    let newRow = document.createElement("tr");
+        leaderboard.appendChild(newRow);
+        newRow.setAttribute('id', 'userInput');
+    const gameData = document.querySelector('#userInput');
         let newName = document.createElement("td");
         newName.innerHTML = userArray[x] + ':';
-        gameData.appendChild(newName)
+        gameData.appendChild(newName);
         let newScore = document.createElement("td");
-        newScore.innerHTML = finalScore
-        console.log(newScore)
+        newScore.innerHTML = finalScore;
+        console.log(newScore);
         gameData.appendChild(newScore);
 
     //create a button that returns back to the original user inout for replayability
     let returnToMenuDiv = document.getElementById("menu-game-screen");
         returnToMenuDiv.setAttribute('onclick', 'resetMenu();');
-        returnToMenuDiv.setAttribute('onmouseenter', 'hover();')
-        returnToMenuDiv.setAttribute('onmouseleave', 'hoverDown();')
+        returnToMenuDiv.setAttribute('onmouseenter', 'hover();');
+        returnToMenuDiv.setAttribute('onmouseleave', 'hoverDown();');
     let returnToMenu = document.getElementById("menu-game-screen");
-    returnToMenu.addEventListener('onclick', resetMenu)
+    returnToMenu.addEventListener('onclick', resetMenu);
 }
 
 function hover(event) {
     if(returnToMenu.style.backgroundImage != null){
         let returnToMenuDiv = document.getElementById("menu-game-screen");
-        returnToMenuDiv.style.scale = '1.05'
+        returnToMenuDiv.style.scale = '1.05';
     }
 }
 function hoverDown(event) {
     if (returnToMenu.style.backgroundImage != null){
         let returnToMenuDiv = document.getElementById("menu-game-screen");
-        returnToMenuDiv.style.scale = '1'
+        returnToMenuDiv.style.scale = '1';
     }
 }
 let returnToMenu = document.getElementById("menu-game-screen");
-    returnToMenu.addEventListener('onmouseenter', hover)
-    returnToMenu.addEventListener('onmouseleave', hoverDown)
+    returnToMenu.addEventListener('onmouseenter', hover);
+    returnToMenu.addEventListener('onmouseleave', hoverDown);
 
 function resetMenu(event) {
     let returnToMenu = document.getElementById("menu-game-screen");
-    returnToMenu.removeAttribute('onclick', 'resetMenu();')
+    returnToMenu.removeAttribute('onclick', 'resetMenu();');
     returnToMenu.removeAttribute('onmouseenter', 'hover();');
     returnToMenu.removeAttribute('onmouseleave', 'hoverDown();');
     returnToMenu.style.scale = '1'
@@ -220,22 +218,22 @@ function resetMenu(event) {
         <label id="user-label" for="username">Username:</label>
         <input id="username" name="username" type="text" required>
     </div>
-    <input  id="submit" type="image" name="submit" alt="submit" aria-label="submit" src="assets/images/pixilart-drawing-(start).png">`
+    <input  id="submit" type="image" name="submit" alt="submit" aria-label="submit" src="assets/images/pixilart-drawing-(start).png">`;
     if(mq.matches){
         let newLogo = document.createElement('header');
         newLogo.innerHTML = `<h1>the touch type game</h1>`;
-        let firstDiv = document.getElementById('fail-div')
-        let body = document.getElementById('home-page')
-        body.insertBefore(newLogo, firstDiv)
+        let firstDiv = document.getElementById('fail-div');
+        let body = document.getElementById('home-page');
+        body.insertBefore(newLogo, firstDiv);
     }
     let screen = document.getElementById("menu-game-screen");
-        screen.style.backgroundImage = null
+        screen.style.backgroundImage = null;
         screen.appendChild(newMenu);
     for (let i = 1; i < 6; i++){
-        console.log(i)
+        console.log(i);
     let failReset = document.getElementsByClassName('fail');
-    failReset.namedItem(i).style.color = "greenyellow"
-    failReset.namedItem(i).style.borderColor = "white"
+    failReset.namedItem(i).style.color = "greenyellow";
+    failReset.namedItem(i).style.borderColor = "white";
     sortTable();
     }
     //Sort table function tken from w3schools credit to https://www.w3schools.com/howto/howto_js_sort_table.asp
